b84c689f829aad9b00c07098d86b7785a4350fcd
Igor Kamyshev <igor@kamyshev.me>
2019-06-27 16:31:19 +0300

    fix: remove extra bind

--- lib/Cache.ts
+++ lib/Cache.ts
@@ -3,7 +3,7 @@ import { CacheProvider } from './provider/CacheProvider'
 export class Cache implements CacheProvider {
   constructor(private readonly provider: CacheProvider) {}
 
-  get = this.provider.get.bind(this.provider)
-  set = this.provider.set.bind(this.provider)
-  reset = this.provider.reset.bind(this.provider)
+  get = this.provider.get
+  set = this.provider.set
+  reset = this.provider.reset
 }
