f0dc618270bb0d98db1f8dbeb8b32242de57c89a
Igor Kamyshev <igor@kamyshev.me>
2019-06-28 11:21:34 +0300

    fix: add miss export

--- lib/index.ts
+++ lib/index.ts
@@ -3,5 +3,6 @@ export { Serializer } from './provider/serializer/Serializer'
 export { CacheProvider } from './provider/CacheProvider'
 export { InMemoryProvider } from './provider/InMemoryProvider'
 export { RedisProvider } from './provider/RedisProvider'
+export { FileSystemProvider } from './provider/FileSystemProvider'
 
 export { Cache } from './Cache'
