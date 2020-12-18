4c0b3b152e063c34a54f32c286e303d3edb71722
Igor Kamyshev <igor@kamyshev.me>
2020-12-18 12:24:33 +0700

    feat(redis): allow use connection string as config

--- lib/provider/RedisProvider.ts
+++ lib/provider/RedisProvider.ts
@@ -1,10 +1,11 @@
 import Redis from 'ioredis'
+import { ConnectionString } from 'connection-string'
 
 import { CacheProvider } from './CacheProvider'
 import { Serializer } from './serializer/Serializer'
 import { defaultSerializer } from './serializer/defaultSerializer'
 
-type Config = Redis.RedisOptions
+type Config = Redis.RedisOptions | string
 
 export class RedisProvider implements CacheProvider {
   private readonly redisClient: Redis.Redis
@@ -13,7 +14,18 @@ export class RedisProvider implements CacheProvider {
     config: Config,
     private readonly serializer: Serializer = defaultSerializer,
   ) {
-    this.redisClient = new Redis(config)
+    if (typeof config === 'string') {
+      const parsed = new ConnectionString(config)
+
+      this.redisClient = new Redis({
+        port: parsed.port,
+        host: parsed.host,
+        username: parsed.user,
+        password: parsed.password,
+      })
+    } else {
+      this.redisClient = new Redis(config)
+    }
   }
 
   get = async <T>(key: string, def?: T) => {
